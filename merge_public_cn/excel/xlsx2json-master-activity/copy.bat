a114c6e0770d190a8a52467f98d03b47ffcf36aa
高高 <gaoyanwen@mijingcn.com>
2026-03-27 19:04:38 +0800

    更新资源

--- merge_public_cn/excel/xlsx2json-master-activity/copy.bat
+++
@@ -1,6 +0,0 @@
-set source_path="..\..\merge_public\excel"
-set destination_path=".\excel"
-
-robocopy %source_path% %destination_path%
-
-pause
--- merge_public_cn/excel/xlsx2json-master-activity/json_format.bat
+++
@@ -1,4 +0,0 @@
-set out_dir=../Client/assets/Config
-
-node.exe json_format.js -f 0 -d %out_dir%
-pause
